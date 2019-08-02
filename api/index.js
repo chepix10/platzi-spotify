ac46de2d45489018dbe9fb01e6afbea90176250f
David Salas <jlmruiz@outlook.com>
2019-08-02 10:27:26 -0600

    Added changes until v. 23

--- api/index.js
+++ api/index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
+const request = require('request')
 const { config } = require('./config/index')
+
+const encodeBasic = require('./utils/encodeBasic')
 
 const app = express()
 
 app.use(bodyParser.json())
+
+const getUserPlaylists = (accessToken, userId) => {
+  if (!accessToken || !userId) {
+    return Promise.resolve(null)
+  }
+
+  const options = {
+    url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true
+  }
+
+  return new Promise((resolve, reject) => {
+    request.get(options, function (error, response, body) {
+      if (error || response.statusCode !== 200) {
+        reject(error)
+      }
+
+      resolve(body)
+    })
+  })
+}
 
 app.post('/api/auth/token', (req, res) => {
   const { email, username, name } = req.body
@@ -14,14 +39,42 @@ app.post('/api/auth/token', (req, res) => {
 })
 
 app.get('/api/auth/verify', (req, res, next) => {
-  const { access_token } = req.query
+  const { access_token: accessToken } = req.query
 
   try {
-    const decoded = jwt.verify(access_token, config.authJwtSecret)
+    const decoded = jwt.verify(accessToken, config.authJwtSecret)
     res.json({ message: 'The access token is valid', username: decoded.sub })
   } catch (err) {
     next(err)
   }
+})
+
+app.get('/api/playlists', async (req, res, next) => {
+  const { userId } = req.query
+
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization: `Basic ${encodeBasic(
+        config.spotifyClientId,
+        config.spotifyClientSecret
+      )}`
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  }
+
+  request.post(authOptions, async (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      next(error)
+    }
+
+    const accessToken = body.access_token
+    const userPlaylists = await getUserPlaylists(accessToken, userId)
+    res.json({ playlists: userPlaylists })
+  })
 })
 
 const server = app.listen(5000, () => {
